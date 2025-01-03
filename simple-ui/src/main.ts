714500dbb6980b273254a90c12ceaa31a254cac0
JavierGmnDEV <javier.gmn@yahoo.com>
2025-01-03 08:01:33 +0100

    1.0.1

---
+++ simple-ui/src/main.ts
@@ -0,0 +1,2 @@
+import "../src/index.css"
+export {Title} from "./Title/Title"
--- simple-ui/vite.config.ts
+++ simple-ui/vite.config.ts
@@ -1,7 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+import dts from 'vite-plugin-dts'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts({rollupTypes: true})],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/main.ts'),
+      name: 'Simple-ui',
+      // the proper extensions will be added
+      fileName: 'Simple-ui',
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['react', 'react-dom', 'react/jsx-runtime'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDom',
+          'react/jsx-runtime': 'react/jsx-runtime',
+
+        },
+      },
+    },
+  },
+  
 })
